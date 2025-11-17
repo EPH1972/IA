5d1d663e3c46dd6a9b69d70465bfb5448f3e7982
EPH1972 <eduardperezherrdia@gmail.com>
2025-11-17 20:13:43 +0100

    Mnist Update

--- RedesNeuronales/7Image/classes.py
+++ RedesNeuronales/7Image/classes.py
@@ -1,11 +1,16 @@
 from abc import abstractmethod
+import numpy as np
 
-#declaration abs 
+#Classes declaration 
 class layer():
-    @abstractmethod
-    def __init__(self,W,B):
-        pass
     
+    def __init__(self, X, W,B):
+        self.X = X
+        self.W = W
+        self.B = B
+        self.dW = 0
+        self.dB = 0
+
     @abstractmethod
     def forward(X):
         pass
@@ -18,3 +23,37 @@ class layer():
     def update(W,B,dW,dB,alfa):
         pass
 
+
+class Densa(layer):
+    
+    def forward(X):
+        y = X @ super().W + super().B
+        return y
+    
+    def backward(ypred, yreal):
+        return super().backward(yreal)
+    
+    def update(W, B, dW, dB, alfa):
+        return super().update(B, dW, dB, alfa)
+
+class ReLU(layer):
+    
+    def forward(X):
+        return np.maximum(X,0)
+    
+    def backward(ypred, yreal):
+        return super().backward(yreal)
+    
+    def update(W, B, dW, dB, alfa):
+        return super().update(B, dW, dB, alfa)
+
+class model():
+    def __init__(self, d1, dact, l1, lact):
+        self.epoch = [d1,dact,l1,lact]
+
+
+    def loss():
+        pass
+
+    def acc():
+        pass 
--- RedesNeuronales/7Image/main.py
+++ RedesNeuronales/7Image/main.py
@@ -1,27 +1,69 @@
 import idx2numpy
-from classes import layer
+import numpy as np
+from classes import Densa
+from classes import ReLU
+from classes import model
 from matplotlib import pyplot as plt
 
 
-# Reading
+''' def train_step(model, x,y,lr):
+    ypred = model.forward(x)
+    loss = (ypred - yreal)
+    model.basckward(loss)
+    model.update(lr)
+    acc = acc(ypred, yreal)
+    print(acc,loss)
 
+def train_loop(model,data,lr):
+    for i in range(epochs):
+        for x,y in data:
+            train_step(model,x,y,lr)'''
+
+
+# Reading
+y_test = idx2numpy.convert_from_file('t10k-labels.idx1-ubyte')
+X_test = idx2numpy.convert_from_file('t10k-images.idx3-ubyte')
 y_train = idx2numpy.convert_from_file('train-labels.idx1-ubyte')
 X_train = idx2numpy.convert_from_file('train-images.idx3-ubyte')
 
 
 images = X_train/256
 
-count = 0
-acc = 0
+X_train = np.reshape(X_train, (60, 1000, 784))
+y_train = np.reshape(y_train, )
+
+def train_step():
+    pass
+
+def init_params():
+    w1=np.random.rand(10,784)-0.5
+    b1=np.random.rand(10,1)-0.5
+    w2=np.random.rand(10,10)-0.5
+    b2=np.random.rand(10,1)-0.5
+    return w1, b1, w2, b2
 
 
 
 
-plt.figure(figsize=(10, 5))
-for i in range(4):
-    plt.subplot(1, 4, i+1)
-    plt.imshow(images[i], cmap='gray')
-    plt.title(f"Label: {y_train[i]}")
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+
+def main():
+    D1 =  Densa()
+    D1Act = ReLU()
+    D2 = Densa()
+    D2Act = ReLU()
+
+    Model = model(D1,D1Act,D2,D2Act)
+    for i in len(Model.epoch):
+        print("In")
+
+    '''plt.figure(figsize=(10, 5))
+    for i in range():
+        plt.subplot(1, 1000, i+1)
+        plt.imshow(images[i], cmap='gray')
+        plt.title(f"Label: {y_train[i]}")
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()'''
+
+if __name__=='__main__':
+    main()
